2ccb271542872da50277dd4251546b7bc6987dfd
Callum Apps <c.apps@student.reading.ac.uk>
2023-04-17 11:40:29 +0000

    Update Map.hpp

--- Map.hpp
+++ Map.hpp
@@ -1,22 +1,29 @@
-// map.hpp
 #ifndef MAP_HPP
 #define MAP_HPP
 
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+class Mushroom;  // forward declaration
+
 class Map {
 public:
-    Map(const std::string& cloudTexturePath, const std::string& blockTexturePath, size_t cloudCount = 20);
+    Map(const std::string& cloudTexturePath, const std::string& blockTexturePath, const std::string& block2TexturePath, size_t cloudCount);
 
     void update(float moveVectorX);
     void draw(sf::RenderWindow& window);
+    bool isOnGround(const sf::Sprite& marioSprite) const;
+    bool checkCollision(const sf::Sprite& sprite, const sf::Vector2f& moveVector) const;
 
 private:
     sf::Texture cloudTexture;
-    std::vector<sf::Sprite> cloudSprites;
+    sf::Texture block2Texture;
     sf::Texture blockTexture;
+    std::vector<sf::Sprite> cloudSprites;
+    std::vector<sf::Sprite> luckysprites;
     std::vector<sf::Sprite> floorBlocks;
+
+    friend class Mushroom;  // allow access to private members
 };
 
 #endif // MAP_HPP
